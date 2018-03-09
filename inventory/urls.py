453327444ab9a028a6509b7a6c462bb2720fc5e8
Trey Piepmeier <trey@treypiepmeier.com>
2018-03-09 09:02:48 -0500

    Adding a page for all films of a type for a user.
    http://morozov.ca/tip-how-to-get-a-single-objects-value-with-django-orm.html

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<username>/', views.profile, name='profile'),
+    path('<username>/<slug:slug>/', views.profile_films, name='profile-films'),
+    path('<username>/<slug:slug>/<int:pk>/', views.profile_film_detail, name='profile-film'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -18,3 +18,19 @@ def profile(request, username):
         'owner': owner,
     }
     return render(request, 'inventory/profile.html', context)
+
+
+def profile_films(request, username, slug):
+    owner = get_object_or_404(User, username=username)
+    films = Film.objects.filter(owner=owner.id, name__slug=slug)
+    name = FilmName.objects.only('name').get(slug=slug)
+    context = {
+        'films': films,
+        'name': name,
+        'owner': owner,
+    }
+    return render(request, 'inventory/films.html', context)
+
+
+def profile_film_detail(request):
+    pass
