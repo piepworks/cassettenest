655cd51812be76b5c0fb489291fd6b5089a4f7c0
Trey Piepmeier <trey@treypiepmeier.com>
2018-08-11 11:48:58 -0400

    Created test to prevent previous problem.
    Rolls should have a set status and code after saving.
    
    http://model-mommy.readthedocs.io/en/latest/
    https://pypi.org/project/model_mommy/
    via https://stackoverflow.com/a/19554959/96257

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from model_mommy import mommy
+from .models import Roll
+from .utils import *
+import datetime
 
-# Create your tests here.
+
+class RollTestCase(TestCase):
+    def test_loaded_rolls_get_code_and_status(self):
+        roll = mommy.make(Roll)
+        roll.started_on = datetime.date.today()
+        roll.save()
+        self.assertEqual(roll.code, '35-c41-1')
+        self.assertEqual(roll.status, status_number('loaded'))
