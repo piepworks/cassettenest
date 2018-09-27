130ed3f5adec7c772ca131e38354090568999a57
Trey Piepmeier <trey@treypiepmeier.com>
2018-09-27 08:59:51 -0400

    Getting settings ready to deploy to a server.
    Symlinking the local_settings.py file from Dropbox. The same place as
    the SQLite database.

--- film/settings.py
+++ film/settings.py
@@ -73,17 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'film.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -142,3 +131,13 @@ STATICFILES_FINDERS = (
 )
 
 LOGIN_URL = '/login/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+try:
+    from .prod_settings import *
+except ImportError:
+    pass
