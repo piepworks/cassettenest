64fe527f24db4cea217f24d52c5e1dd405cfed9a
Trey Piepmeier <trey@treypiepmeier.com>
2018-10-14 11:16:11 -0400

    Test to make sure Roll code sequence works.
    It does, I had an issue with how I manually entered my existing data.
    I had two rolls of 120 film that were incorrectly set as 35mm. Now
    things seem to work as expected.

--- inventory/tests.py
+++ inventory/tests.py
@@ -12,3 +12,47 @@ class RollTestCase(TestCase):
         roll.save()
         self.assertEqual(roll.code, '35-c41-1')
         self.assertEqual(roll.status, status_number('loaded'))
+
+    def test_rolls_get_correct_code_sequence(self):
+        roll1 = mommy.make(
+            Roll,
+            owner__id=1,
+            film__type='e6',
+            started_on=datetime.date.today()
+        )
+        roll2 = mommy.make(
+            Roll,
+            owner__id=1,
+            film__type='c41',
+            started_on=datetime.date.today()
+        )
+        roll3 = mommy.make(
+            Roll,
+            owner__id=1,
+            film__type='e6',
+            started_on=datetime.date.today()
+        )
+        roll4 = mommy.make(
+            Roll,
+            owner__id=1,
+            film__type='c41',
+            started_on=datetime.date.today()
+        )
+        roll5 = mommy.make(
+            Roll,
+            owner__id=1,
+            film__type='bw',
+            started_on=datetime.date.today()
+        )
+
+        roll1.save()
+        roll2.save()
+        roll3.save()
+        roll4.save()
+        roll5.save()
+
+        self.assertEqual(roll1.code, '35-e6-1')
+        self.assertEqual(roll2.code, '35-c41-1')
+        self.assertEqual(roll3.code, '35-e6-2')
+        self.assertEqual(roll4.code, '35-c41-2')
+        self.assertEqual(roll5.code, '35-bw-1')
