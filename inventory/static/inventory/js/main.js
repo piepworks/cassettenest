944493386cc5c9268592f496ed8206c64d9035b6
Trey Piepmeier <trey@treypiepmeier.com>
2019-06-06 21:36:12 -0400

    Quick and dirty JavaScript date picker
    https://github.com/nazar-pc/PickMeUp
    
    I want to make sure this meets accessibility standards, but it seems to
    do the job for now.

--- inventory/static/inventory/js/main.js
+++ inventory/static/inventory/js/main.js
@@ -5,3 +5,12 @@ const confirmDelete = function(item='item') {
 const confirmRemove = function(item='item') {
     return confirm(`Are you sure you want to remove this ${item}?`);
 }
+
+const pickmeupOptions = {
+    format: 'Y-m-d',
+    default_date: false,
+    hide_on_select: true,
+}
+
+pickmeup('#id_started_on', pickmeupOptions);
+pickmeup('#id_ended_on', pickmeupOptions);
