89c2c4402f19f110ad6301bdf5ed4fb718aa08de
Trey Piepmeier <trey@treypiepmeier.com>
2019-06-20 19:25:48 -0400

    Remove JS datepicker
    It was being twitchy and it's not necessary yet.

--- inventory/static/inventory/js/main.js
+++ inventory/static/inventory/js/main.js
@@ -5,23 +5,3 @@ const confirmDelete = function(item='item') {
 const confirmRemove = function(item='item') {
     return confirm(`Are you sure you want to remove this ${item}?`);
 }
-
-const dateWidgetSupported = function() {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'date');
-    const support = input.type !== 'text';
-    delete input;
-    return support;
-}
-
-if (!dateWidgetSupported()) {
-    const pickmeupOptions = {
-        format: 'm/d/Y',
-        default_date: false,
-        hide_on_select: true,
-    }
-
-    pickmeup('#id_date', pickmeupOptions);
-    pickmeup('#id_started_on', pickmeupOptions);
-    pickmeup('#id_ended_on', pickmeupOptions);
-}
