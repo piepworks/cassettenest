3ca92cd4071854f3c4357d48d0d5b046f518dc19
Trey Piepmeier <trey@treypiepmeier.com>
2019-07-13 19:15:39 -0400

    Keep git hook here for safe keeping

---
+++ post-receive-hook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# https://blog.pythonanywhere.com/87/
+GIT_WORK_TREE=/home/treypiepmeier/film git checkout -f
+
+now=$(date +"%F_%H-%M-%s")
+backup_file="../backups/cassettenest_$now.json"
+
+cd $HOME/film
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+workon film
+./manage.py dumpdata > $backup_file
+./manage.py collectstatic --noinput
+./manage.py migrate --noinput
+deactivate
+
+touch /var/www/app_cassettenest_com_wsgi.py
