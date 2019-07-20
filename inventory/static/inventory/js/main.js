94fbda50b5a95314bc0efaed0e696ac6d35e84c8
Trey Piepmeier <trey@treypiepmeier.com>
2019-07-20 12:22:45 -0400

    Fairly big overhaul of camera load page
    - New form styles
    - New field type for multiple inputs
    - Toggles of the two filters (terrible temporary toggle styles)
    - Filters are visible without JS
    - Cleaner Sass
    
    https://codepen.io/hidanielle/post/level-up-your-sass-with-the-ampersand
    
    https://css-tricks.com/snippets/sass/caching-current-selector-sass/
    
    https://www.tutorialrepublic.com/faq/how-to-toggle-text-inside-an-element-on-click-using-jquery.php

--- inventory/static/inventory/js/main.js
+++ inventory/static/inventory/js/main.js
@@ -5,3 +5,14 @@ const confirmDelete = function(item='item') {
 const confirmRemove = function(item='item') {
     return confirm(`Are you sure you want to remove this ${item}?`);
 }
+
+$('html').addClass('has-js');
+
+$('.toggle-button').click(function(e) {
+    e.preventDefault();
+    let $this = $(this);
+    let $toggle = $this.closest('.c-field').next('.toggle');
+
+    $this.text($this.text() === 'Show' ? 'Hide' : 'Show');
+    $toggle.toggle();
+});
