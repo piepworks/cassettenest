8997eb38de486d13e457d603f037bab82d7786c1
Trey Piepmeier <trey@treypiepmeier.com>
2019-09-27 18:41:37 -0400

    Allow users to change their password
    Fixes #102
    
    https://www.youtube.com/watch?v=mJkwKsSnWXo
    
    https://docs.djangoproject.com/en/2.2/topics/auth/default/#django.contrib.auth.views.PasswordChangeView

--- film/urls.py
+++ film/urls.py
@@ -4,10 +4,17 @@ from django.contrib.auth import views as auth_views
 from inventory import views
 
 urlpatterns = [
+    # Users
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 
     # Static pages
     path('patterns/', views.patterns, name='patterns'),
