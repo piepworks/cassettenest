2b311a48ee53221bef7b662de41f1711c0cc6773
Trey Piepmeier <trey@treypiepmeier.com>
2019-10-05 09:21:19 -0400

    Update scripts
    - Make sure daily-backup script is always marked as executable.
    - Add thorough documentation to both scripts.
    
    https://git-scm.com/docs/git-checkout#Documentation/git-checkout.txt--f
    
    https://blog.pythonanywhere.com/87/

--- scripts/daily-backup.sh
+++ scripts/daily-backup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+
+# Set variables
 now=$(date +"%F_%H-%M-%s")
 backup_file="../backups/cassettenest_$now.json"
 
+# BACKUP THE DATABASE
+# -------------------
+# Go to the live code folder.
 cd $HOME/film
+# Start up virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
+# Activate the `film` virtualenv.
 workon film
+# Actually backup the database.
 ./manage.py dumpdata > $backup_file
+# Deactivate the `film` virtualenv
 deactivate
+# -------------------
 
+# Send backup file to DigitalOcean.
 s3cmd put $backup_file s3://cassettenest/backups-daily/
--- scripts/post-receive-hook.sh
+++ scripts/post-receive-hook.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 # https://blog.pythonanywhere.com/87/
 # I live in ~/bare-repos/cassettenest.git/hooks/post-receive
+
+# Update the live code. The `-f` is to force git to blow away any differences in
+# that codebase to what just came in from the git push.
 GIT_WORK_TREE=/home/treypiepmeier/film git checkout -f
 
+# Set variables
 now=$(date +"%F_%H-%M-%s")
 backup_file="../backups/cassettenest_$now.json"
 
+# BACKUP THE DATABASE
+# -------------------
+# Go to the live code folder.
 cd $HOME/film
+# Start up virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
+# Activate the `film` virtualenv.
 workon film
+# Install any new Python dependencies .
 pip install -r requirements.txt
+# Actually backup the database.
 ./manage.py dumpdata > $backup_file
+# Process static files like Sass
 ./manage.py collectstatic --noinput
+# Run a database migration if it's needed
 ./manage.py migrate --noinput
+# Deactivate the `film` virtualenv
 deactivate
+# -------------------
 
+# Send backup file to DigitalOcean.
 s3cmd put $backup_file s3://cassettenest/backups-code-push/
 
+# Make sure daily backup script is executable.
+chmod +x scripts/daily-backup.sh
+
+# Reboot the app
 touch /var/www/app_cassettenest_com_wsgi.py
