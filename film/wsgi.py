9d56cf2c210324413608f1a85c2ab3b3da23aee5
Trey Piepmeier <trey@treypiepmeier.com>
2019-10-18 08:16:39 -0400

    Save the working bits of PythonAnywhere wsgi's file to actual wsgi.py
    Issue #117
    
    It would be cool to get PythonAnywhere to actually run the version in
    this project, but this is good for now. I'm learning a lot about how
    these things are wired up, but I don't have the bravery to mess with
    changing things again on a Friday morning that I'm supposed to be on
    vacation.

--- film/wsgi.py
+++ film/wsgi.py
@@ -4,13 +4,24 @@ WSGI config for film project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+
+I live here: https://www.pythonanywhere.com/web_app_setup/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "film.settings")
+# https://help.pythonanywhere.com/pages/environment-variables-for-web-apps/
+project_folder = os.path.expanduser('~/film/film')
+load_dotenv(os.path.join(project_folder, '.env'))
+
+path = '/home/treypiepmeier/film'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'film.settings'
 
 application = get_wsgi_application()
--- scripts/app_cassettenest_com_wsgi.py
+++ scripts/app_cassettenest_com_wsgi.py
@@ -1,126 +1,17 @@
-# ----------------------------------------------------------
-# I live here: https://www.pythonanywhere.com/web_app_setup/
-# ----------------------------------------------------------
-
-
-# This file contains the WSGI configuration required to serve up your
-# web application at http://treypiepmeier.pythonanywhere.com/
-# It works by setting the variable 'application' to a WSGI handler of some
-# description.
+# TODO: Try something like this from PythonAnywhere and use the actual wsgi.py
+# file in this project.
 #
-
-# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
-# getting imports and sys.path right can be fiddly!
-# We've tried to collect some general tips here:
-# https://www.pythonanywhere.com/wiki/DebuggingImportError
-
-
-# +++++++++++ HELLO WORLD +++++++++++
-# A little pure-wsgi hello world we've cooked up, just
-# to prove everything works.  You should delete this
-# code to get your own working.
-
-
-# HELLO_WORLD = """<html>
-# <head>
-#     <title>Python Anywhere hosted web application</title>
-# </head>
-# <body>
-# <h1>Hello, World!</h1>
-# <p>
-#     This is the default welcome page for a
-#     <a href="https://www.pythonanywhere.com/">PythonAnywhere</a>
-#     hosted web application.
-# </p>
-# <p>
-#     Find out more about how to configure your own web application
-#     by visiting the <a href="https://www.pythonanywhere.com/web_app_setup/">web app setup</a> page
-# </p>
-# </body>
-# </html>"""
-
-
-# def application(environ, start_response):
-#     if environ.get('PATH_INFO') == '/':
-#         status = '200 OK'
-#         content = HELLO_WORLD
-#     else:
-#         status = '404 NOT FOUND'
-#         content = 'Page not found.'
-#     response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
-#     start_response(status, response_headers)
-#     yield content.encode('utf8')
-
-
-# Below are templates for Django and Flask.  You should update the file
-# appropriately for the web framework you're using, and then
-# click the 'Reload /yourdomain.com/' button on the 'Web' tab to make your site
-# live.
-
-# +++++++++++ VIRTUALENV +++++++++++
-# If you want to use a virtualenv, set its path on the web app setup tab.
-# Then come back here and import your application object as per the
-# instructions below
+# I don't know what the `my_wsgi_file` should actually be.
 
 
 # +++++++++++ CUSTOM WSGI +++++++++++
 # If you have a WSGI file that you want to serve using PythonAnywhere, perhaps
 # in your home directory under version control, then use something like this:
 #
-#import sys
+# import sys
 #
-#path = '/home/treypiepmeier/path/to/my/app
-#if path not in sys.path:
-#    sys.path.append(path)
+# path = '/home/treypiepmeier/path/to/my/app
+# if path not in sys.path:
+#     sys.path.append(path)
 #
-#from my_wsgi_file import application  # noqa
-
-
-# +++++++++++ DJANGO +++++++++++
-# To use your own django app use code like this:
-import os
-import sys
-
-# https://help.pythonanywhere.com/pages/environment-variables-for-web-apps/
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/film/film')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
-
-# assuming your django settings file is at '/home/treypiepmeier/mysite/mysite/settings.py'
-# and your manage.py is is at '/home/treypiepmeier/mysite/manage.py'
-path = '/home/treypiepmeier/film'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'film.settings'
-
-# then:
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-
-
-# +++++++++++ FLASK +++++++++++
-# Flask works like any other WSGI-compatible framework, we just need
-# to import the application.  Often Flask apps are called "app" so we
-# may need to rename it during the import:
-#
-#
-#import sys
-#
-## The "/home/treypiepmeier" below specifies your home
-## directory -- the rest should be the directory you uploaded your Flask
-## code to underneath the home directory.  So if you just ran
-## "git clone git@github.com/myusername/myproject.git"
-## ...or uploaded files to the directory "myproject", then you should
-## specify "/home/treypiepmeier/myproject"
-#path = '/home/treypiepmeier/path/to/flask_app_directory'
-#if path not in sys.path:
-#    sys.path.append(path)
-#
-#from main_flask_app_file import app as application  # noqa
-#
-# NB -- many Flask guides suggest you use a file called run.py; that's
-# not necessary on PythonAnywhere.  And you should make sure your code
-# does *not* invoke the flask development server with app.run(), as it
-# will prevent your wsgi file from working.
+# from my_wsgi_file import application  # noqa
