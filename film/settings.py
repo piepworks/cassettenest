a9c514fc734b2c2e30953c271ef92d172317675b
Trey Piepmeier <trey@treypiepmeier.com>
2020-08-19 07:50:25 -0400

    Got PostgreSQL installed and working locally.
    Going by:
    https://djangoforprofessionals.com/postgresql/
    https://github.com/trey/postgres-example
    
    I’m going to migrate the existing (local) data as a next step.

--- film/settings.py
+++ film/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'film.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, os.environ.get('DATABASE_FILE'))
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
