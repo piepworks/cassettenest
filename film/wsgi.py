12bf7e5bcd1efe464400d2448157698a1d858e23
Trey Piepmeier <trey@treypiepmeier.com>
2020-09-07 15:12:28 -0400

    Install Gunicorn.
    Also revert wsgi.py to it’s default setup. Going by what’s working in
    cn-tools and https://github.com/wsvincent/djangoforprofessionals_30/blob/9ccd563948bf4782ede2b3e206f34ab1f93f2940/ch18-deployment/bookstore_project/wsgi.py.
    
    Chapter 18 (Deployment) of Django for Professionals.

--- film/wsgi.py
+++ film/wsgi.py
@@ -5,23 +5,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-
-I live here: https://www.pythonanywhere.com/web_app_setup/
 """
 
 import os
-import sys
+
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
-# https://help.pythonanywhere.com/pages/environment-variables-for-web-apps/
-project_folder = os.path.expanduser('~/film/film')
-load_dotenv(os.path.join(project_folder, '.env'))
-
-path = '/home/treypiepmeier/film'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'film.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'film.settings')
 
 application = get_wsgi_application()
