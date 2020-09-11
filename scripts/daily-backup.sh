db649902ee87194665a2c8ca7cc8fb9cde5d0343
Trey Piepmeier <trey@treypiepmeier.com>
2020-09-10 22:21:34 -0400

    Update automated backup for the new setup.

--- scripts/daily-backup.sh
+++ scripts/daily-backup.sh
@@ -2,22 +2,11 @@
 
 # Set variables
 now=$(date +"%F_%H-%M-%s")
-backup_file="../backups/cassettenest_$now.json"
+backup_file="$HOME/backups/cassettenest_digitalocean_$now.json"
 
-# BACKUP THE DATABASE
-# -------------------
-# Go to the live code folder.
-cd $HOME/film
-# Start up virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-# Activate the `film` virtualenv.
-workon film
-# Actually backup the database.
-./manage.py dumpdata > $backup_file
-# Deactivate the `film` virtualenv
-deactivate
-# -------------------
+# Backup the database.
+cd $HOME/apps/cassettenest
+docker-compose exec web python manage.py dumpdata > $backup_file
 
-# Send backup file to DigitalOcean.
+# Send backup file to DigitalOcean Space.
 s3cmd put $backup_file s3://cassettenest/backups-daily/
