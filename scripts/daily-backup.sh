2f631ffda8cfb3bd64b5fe49b13326b2970d5c90
Trey Piepmeier <trey@treypiepmeier.com>
2021-01-25 18:15:25 -0500

    Update scripts.
    - Delete older backups on the server, but leave the latest one.
    - Create a temp folder if needed for local backup verification.

--- scripts/daily-backup.sh
+++ scripts/daily-backup.sh
@@ -6,11 +6,12 @@ backup_path="${HOME}/backups"
 backup_file="cassettenest_${now}.dump"
 file_w_path="${backup_path}/${backup_file}"
 
+# Delete any older backups.
+rm ${backup_path}/*.dump
+
 # Backup the database.
 cd $HOME/apps/cassettenest
 /usr/local/bin/docker-compose exec -T db pg_dump --format=custom -U cassette_nest cassette_nest > $file_w_path
 
 # Send backup file to DigitalOcean Space.
 s3cmd put $file_w_path s3://cassettenest/backups-daily/${backup_file}.enc -e
-# Delete the backup file from the server when we’re done.
-rm $file_w_path
