18cce9d811335aaaf6f344c788ceed908add13ca
Trey Piepmeier <trey@treypiepmeier.com>
2021-01-25 14:32:14 -0500

    Get production database running locally!
    And the the local database re-loaded afterwards.
    
    Seems to have worked without any problems. I hope I don’t have to
    restore a database backup in production anytime soon, but I feel much
    more prepared for it now.
    
    I needed to install `gpg` with homebrew after installing s3cmd and then
    update the path in `~.s3cfg`.
    
    https://www.digitalocean.com/docs/spaces/resources/s3cmd/#optional-set-an-encryption-password
    
    shell script to remove a file if it already exist:
    https://stackoverflow.com/a/31318262
    
    How to restore a postgres backup - ERROR: cannot drop the currently
    open database:
    https://stackoverflow.com/a/59083174
    
    The following helped me realize that I could just use `<` to send the
    .dump file to the container rather than copying it over or something. I
    wasn’t sure since I didn’t know if the custom format dump was like a
    binary file or something that couldn’t be output/input like text. I
    guess it’s all good, though.
    
    pg_dump and restore a db in a container:
    http://durandom.de/docker/postgres/2016/12/20/pg_dump/
    
    The following was somewhat helpful, but not exactly what I ended
    up doing.
    
    How to pull production data into your local Postgres database:
    https://mattsegal.dev/restore-django-local-database.html

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Download a file specified on the command line.
-# https://www.digitalocean.com/docs/spaces/resources/s3cmd-usage/#get-one-file
-# It should be decrypted automatically.
-# https://www.digitalocean.com/docs/spaces/resources/s3cmd-usage/#encrypt-files
-# Something like:
-# s3cmd get s3://cassettenest/[folder]/backup.dump.enc backup.dump
+# s3cmd get s3://cassettenest/[folder]/$1.dump.enc $1.dump
 
-# Unzip and restore the database.
+# Restore the database.
 # Something like:
-# docker-compose exec -T db pg_restore -d [dbname] --clean --create backup.dump
+# docker-compose exec -T db pg_restore -U [username] -d [dbname] --clean < $1.dump
