484bdc30d341f61c24aef621da9ac741a080586c
Trey Piepmeier <trey@treypiepmeier.com>
2021-03-19 15:56:33 -0400

    Markdown enhancements can work on all textareas.
    (More than one on per page.)
    
    Also jQuery-ify everything in the JS file.
    
    https://learn.jquery.com/using-jquery-core/faq/how-do-i-pull-a-native-dom-element-from-a-jquery-object/

--- inventory/templates/js/markdown-autocomplete.js
+++ inventory/templates/js/markdown-autocomplete.js
@@ -1,31 +1,32 @@
-const theTextarea = document.getElementById('{{ textarea }}');
-const $theTextarea = $(theTextarea);
+const $textarea = $('textarea');
 const ulRegex = /^- .+/;
 const olRegex = /^\d+\. .+/;
 const olNumberRegex = /^\d+/;
 const emptyLiRegex = /^(-|(\d+\.)) $/;
 const emptyOlLiRegex = /^\d+\. $/;
 
-$theTextarea.on('keyup', e => {
-    // `13` is the return key.
+$textarea.on('keyup', e => {
+    const $this = $(e.target);
+
     if (e.which === 13) {
-        const cursorPosition = $theTextarea.prop('selectionStart');
-        const everything = $theTextarea.val();
+        // `13` is the return key.
+        const cursorPosition = $this.prop('selectionStart');
+        const everything = $this.val();
         const beforeCursor = everything.slice(0, cursorPosition);
         const afterCursor = everything.slice(cursorPosition);
         let lines = beforeCursor.split('\n');
         const lastLine = lines[lines.length - 2];
 
         if (lastLine.match(ulRegex)) {
-            $theTextarea.val(beforeCursor + '- ' + afterCursor);
-            theTextarea.setSelectionRange(cursorPosition + 2, cursorPosition + 2);
+            $this.val(beforeCursor + '- ' + afterCursor);
+            $this[0].setSelectionRange(cursorPosition + 2, cursorPosition + 2);
         }
 
         if (lastLine.match(olRegex)) {
             let nextNumber = Number(olNumberRegex.exec(lastLine)[0]) + 1;
             let numberLength = nextNumber.toString().length + 2;
-            $theTextarea.val(beforeCursor + `${nextNumber}. ` + afterCursor);
-            theTextarea.setSelectionRange(cursorPosition + numberLength, cursorPosition + numberLength);
+            $this.val(beforeCursor + `${nextNumber}. ` + afterCursor);
+            $this[0].setSelectionRange(cursorPosition + numberLength, cursorPosition + numberLength);
         }
 
         if (lastLine.match(emptyLiRegex)) {
@@ -36,8 +37,8 @@ $theTextarea.on('keyup', e => {
             }
 
             lines.splice(-2, 1);
-            $theTextarea.val(lines.join('\n') + '' + afterCursor);
-            theTextarea.setSelectionRange(cursorPosition - offset, cursorPosition - offset);
+            $this.val(lines.join('\n') + '' + afterCursor);
+            $this[0].setSelectionRange(cursorPosition - offset, cursorPosition - offset);
         }
     }
 });
