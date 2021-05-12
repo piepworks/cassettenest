2a6c7910a64c8780f5d9f4fb0006da3693def599
Trey Piepmeier <trey@treypiepmeier.com>
2021-05-12 12:59:13 -0400

    Add a nice str to Frame.
    Now that we can see it in the admin…

--- inventory/models.py
+++ inventory/models.py
@@ -519,3 +519,6 @@ class Frame(models.Model):
     shutter_speed = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Frame #{self.number} of {self.roll}'
