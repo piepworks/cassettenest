51459e1e9444178894d609c768a0d4c6f354f617
Trey Piepmeier <trey@treypiepmeier.com>
2021-05-12 15:52:03 -0400

    Flesh out frame detail page.

--- inventory/views.py
+++ inventory/views.py
@@ -1514,7 +1514,8 @@ def roll_frame_add(request, roll_pk):
 def roll_frame_detail(request, roll_pk, number):
     frame = get_object_or_404(Frame, roll__id=roll_pk, roll__owner=request.user, number=number)
     context = {
-        'frame': frame
+        'frame': frame,
+        'js_needed': True,
     }
 
     return render(
