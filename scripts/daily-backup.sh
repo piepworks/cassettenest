ba44e6cfbd31bae77694109bcccd558057a26599
Trey Piepmeier <trey@treypiepmeier.com>
2021-11-03 11:37:10 -0400

    Update backup scripts.
    BashFAQ/003 - Greg's Wiki:
    http://mywiki.wooledge.org/BashFAQ/003
    
    via :
    linux - Bash function to find newest file matching pattern:
    https://stackoverflow.com/a/5886002

--- scripts/daily-backup.sh
+++ scripts/daily-backup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Set variables
-now=$(date +"%F")
-backup_path="${HOME}/backups"
-backup_file="cassettenest_${now}.dump"
-file_w_path="${backup_path}/${backup_file}"
+backup_path="${HOME}/apps/cassettenest/backups"
 
 # Delete any older backups.
 rm ${backup_path}/*.dump 2> /dev/null
 
 # Backup the database.
 cd $HOME/apps/cassettenest
-/usr/local/bin/docker-compose exec -T db pg_dump --format=custom -U cassette_nest cassette_nest > $file_w_path
+/usr/local/bin/docker-compose exec -T web python manage.py dbbackup -z
+
+# Find the backup file we just created.
+unset -v latest
+for file in "$backup_path"/*; do
+  [[ $file -nt $latest ]] && latest=$file
+done
+backup_file=$latest
+file_w_path="${backup_path}/${backup_file}"
 
 # Send backup file to DigitalOcean Space.
 s3cmd put $file_w_path s3://cassettenest/backups-daily/${backup_file}.enc -e
