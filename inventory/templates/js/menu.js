afd25c26e276159cdac78b380e759b556429ebeb
Trey Piepmeier <trey@treypiepmeier.com>
2022-08-02 22:25:01 -0400

    Prevent errors with redefining variables.
    When using push state. Thank you to Josh Mock and Marc Sexton for
    talking me through this stuff.
    
    This feels a lot cleaner.
    
    This is new and exciting to me. I'm going to learn JS because I hate
    modern JS.
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects#defining_getters_and_setters
    
    via …
    
    How do I reference the same Object's properties during its creation?
    https://stackoverflow.com/a/42437104
    
    https://stackoverflow.com/questions/3352020/how-to-declare-a-global-variable-in-javascript
    
    JavaScript check if variable exists (is defined/initialized)
    https://stackoverflow.com/a/519157

--- inventory/templates/js/menu.js
+++ inventory/templates/js/menu.js
@@ -1,51 +1,57 @@
-/* eslint-disable no-unused-vars */
 /* global htmx */
-const menu = document.querySelector('#menu');
-const menuInner = document.querySelector('#menu-inner');
-const menuOverlay = document.querySelector('#menu-overlay');
-const menuContent = document.querySelector('#menu-content');
-const openMenuButton = document.querySelector('#menu-open');
-const closeMenuButtonWrapper = document.querySelector('#menu-close');
-const closeMenuButton = closeMenuButtonWrapper.querySelector('button');
-const desktopToggleButton = document.querySelector('#menu-desktop-toggle');
+if (typeof window.cnMenu === 'undefined') {
+  window.cnMenu = {
+    base: document.querySelector('#menu'),
+    inner: document.querySelector('#menu-inner'),
+    overlay: document.querySelector('#menu-overlay'),
+    content: document.querySelector('#menu-content'),
+    openButton: document.querySelector('#menu-open'),
+    closeButtonWrapper: document.querySelector('#menu-close'),
+    get closeButton() { return this.closeButtonWrapper.querySelector('button'); },
+    toggleButton: document.querySelector('#menu-desktop-toggle'),
 
-const setupMenu = () => {
-  menu.classList.add('-translate-x-full', 'fixed');
-  menuInner.classList.add('min-h-screen');
-  menuOverlay.classList.add('opacity-0', 'fixed', 'inset-0', 'bg-opacity-75');
-  menuContent.classList.add('-translate-x-full', 'max-w-xs');
-  openMenuButton.classList.remove('hidden');
-  openMenuButton.classList.add('inline-flex');
-  closeMenuButtonWrapper.classList.remove('hidden');
-  desktopToggleButton.classList.add('md:tall:flex');
-};
-const openMenu = () => {
-  document.body.classList.add('overflow-hidden');
-  menu.classList.remove('-translate-x-full');
-  menuOverlay.classList.remove('opacity-0');
-  menuOverlay.setAttribute('aria-hidden', 'false');
-  menu.setAttribute('aria-hidden', 'false');
-  menuContent.classList.remove('-translate-x-full');
-  setTimeout(() => {
-    closeMenuButton.classList.remove('hidden');
-  }, 100);
-};
-const closeMenu = () => {
-  document.body.classList.remove('overflow-hidden');
-  menuOverlay.classList.add('opacity-0');
-  menuOverlay.setAttribute('aria-hidden', 'true');
-  menu.setAttribute('aria-hidden', 'true');
-  menuContent.classList.add('-translate-x-full');
-  setTimeout(() => {
-    closeMenuButton.classList.add('hidden');
-  }, 100);
-  setTimeout(() => {
-    menu.classList.add('-translate-x-full');
-  }, 300);
-};
-const desktopToggle = () => {
-  menuInner.classList.toggle('collapsed');
-  htmx.ajax('GET', '{% url "session-sidebar" %}', { swap: 'none' });
-};
+    setupMenu: function() {
+      this.base.classList.add('-translate-x-full', 'fixed');
+      this.inner.classList.add('min-h-screen');
+      this.overlay.classList.add('opacity-0', 'fixed', 'inset-0', 'bg-opacity-75');
+      this.content.classList.add('-translate-x-full', 'max-w-xs');
+      this.openButton.classList.remove('hidden');
+      this.openButton.classList.add('inline-flex');
+      this.closeButtonWrapper.classList.remove('hidden');
+      this.toggleButton.classList.add('md:tall:flex');
+    },
 
-setupMenu();
+    openMenu: function() {
+      document.body.classList.add('overflow-hidden');
+      this.base.classList.remove('-translate-x-full');
+      this.overlay.classList.remove('opacity-0');
+      this.overlay.setAttribute('aria-hidden', 'false');
+      this.base.setAttribute('aria-hidden', 'false');
+      this.content.classList.remove('-translate-x-full');
+      setTimeout(() => {
+        this.closeButton.classList.remove('hidden');
+      }, 100);
+    },
+
+    closeMenu: function() {
+      document.body.classList.remove('overflow-hidden');
+      this.overlay.classList.add('opacity-0');
+      this.overlay.setAttribute('aria-hidden', 'true');
+      this.base.setAttribute('aria-hidden', 'true');
+      this.content.classList.add('-translate-x-full');
+      setTimeout(() => {
+        this.closeButton.classList.add('hidden');
+      }, 100);
+      setTimeout(() => {
+        this.base.classList.add('-translate-x-full');
+      }, 300);
+    },
+
+    desktopToggle: function() {
+      this.inner.classList.toggle('collapsed');
+      htmx.ajax('GET', '{% url "session-sidebar" %}', { swap: 'none' });
+    },
+  };
+
+  window.cnMenu.setupMenu();
+}
