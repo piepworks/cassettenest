34d8e37ba7673f03011955bb4019b134bc612e6c
Trey Piepmeier <trey@treypiepmeier.com>
2022-10-16 11:45:46 -0400

    Fix failing, nascent Playwright test.

--- e2e/signup.spec.ts
+++ e2e/signup.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('signup page is awesome', async ({ page }) => {
-  await page.goto('https://v2.cassettenest.com');
+  await page.goto('https://app.cassettenest.com');
   await expect(page).toHaveTitle("Log in / Cassette Nest");
   const signupLink = page.locator('text=start your 14-day free trial');
   await expect(signupLink).toHaveAttribute('href', '/register/');
