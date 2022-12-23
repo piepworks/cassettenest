92464d416ed34479e8428318407e2754b441dea8
Trey Piepmeier <trey@treypiepmeier.com>
2022-12-23 13:20:08 -0500

    Fix static path.
    https://docs.djangoproject.com/en/4.1/ref/templates/builtins/#get-static-prefix

--- inventory/templates/js/sw.js
+++ inventory/templates/js/sw.js
@@ -3,7 +3,7 @@
 importScripts(`{% static 'js/vendor/workbox-v6.5.4/workbox-sw.js' %}`);
 
 workbox.setConfig({
-  modulePathPrefix: `{% static 'js/vendor/workbox-v6.5.4' %}`,
+  modulePathPrefix: '{% get_static_prefix %}js/vendor/workbox-v6.5.4',
 });
 
 workbox.routing.registerRoute(
