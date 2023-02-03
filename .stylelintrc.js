4d7a93b72603a6361e37c3abdbc0802a28e72aff
Trey Piepmeier <trey@treypiepmeier.com>
2023-02-03 10:51:51 -0500

    Update Stylelint to work with Tailwind `theme()`.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,9 +3,31 @@ module.exports = {
   'rules': {
     'indentation': 2,
     'string-quotes': 'single',
-    'at-rule-no-unknown': null,
     'declaration-empty-line-before': 'never',
     'no-descending-specificity': null,
+    // Tailwind-related rules:
+    // -----------------------
+    'function-no-unknown': [
+      true,
+      { ignoreFunctions: ['theme'] }
+    ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ]
+      }
+    ],
+    'at-rule-empty-line-before': null,
+    'value-keyword-case': null,
+    'number-leading-zero': null,
+    'number-no-trailing-zeros': null,
+    // -----------------------
   },
   'ignoreFiles': ['static/tailwind.css'],
 };
