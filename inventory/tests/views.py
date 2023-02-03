990bd4db231c8b4e0c46f46714d57b46934d3c4d
Trey Piepmeier <trey@treypiepmeier.com>
2023-02-03 17:22:13 -0500

    Get test to pass.
    Closes #324.

--- inventory/tests/views.py
+++ inventory/tests/views.py
@@ -195,6 +195,7 @@ class SettingsTests(TestCase):
                 "first_name": "Frank",
                 "last_name": "Poole",
                 "email": "frank@example.com",
+                "color_preference": "auto",
             },
         )
         messages = [m.message for m in get_messages(response.wsgi_request)]
