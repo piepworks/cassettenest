005fb4bc4b8c8d2f55bab193103d56de5b1769ce
Trey Piepmeier <trey@treypiepmeier.com>
2023-07-07 11:10:05 -0400

    Test registration
    This is still very basic. Just getting a feel for how to use Playwright
    with the test server (instead of a locally running server like the
    examples all show) so that this is ready to be plugged into a GitHub
    workflow.
    
    https://stackoverflow.com/questions/2030053/how-to-generate-random-strings-in-python
    
    Started out with this stuff:
    https://playwright.dev/python/docs/codegen-intro
    
    Another great example from this repo:
    https://github.com/Chris-May/django_playwright_pytest_example/blob/eec89612e36ace05fb80874bf5f2f147a3f2a590/tests/test_ui.py#L12

--- inventory/tests/test_ui.py
+++ inventory/tests/test_ui.py
@@ -1,8 +1,33 @@
 import re
-from playwright.sync_api import expect
 import pytest
+import random
+import string
+from playwright.sync_api import expect
 
 
 @pytest.mark.playwright()
 def test_login_page(test_server):
     expect(test_server).to_have_title(re.compile("Log in / Cassette Nest"))
+
+
+@pytest.mark.playwright()
+def test_create_account(test_server):
+    test_server.get_by_role("link", name="start your 14-day free trial").click()
+    username = "".join(random.choice(string.ascii_letters) for i in range(5))
+    test_server.get_by_label("Username").fill(username)
+    test_server.get_by_label("Email").fill(f"{username}@example.com")
+    password = "".join(
+        random.choice(string.ascii_letters + string.digits + string.punctuation)
+        for i in range(12)
+    )
+    test_server.get_by_label("Password", exact=True).fill(password)
+    test_server.get_by_label("Password confirmation").fill(password)
+    test_server.get_by_role("button", name="Create your account").click()
+
+    subscription_banner = test_server.locator(".subscription-banner")
+    expect(subscription_banner).to_contain_text(
+        "You have 14 days left in your free trial."
+    )
+
+
+# TODO: test film stock dropdown htmx stuff
