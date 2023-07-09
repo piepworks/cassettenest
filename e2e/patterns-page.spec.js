6ddeab06dace65ea29d37de7d3ea5605e903813e
Trey Piepmeier <trey@treypiepmeier.com>
2023-07-09 18:52:25 -0400

    Set up visual regression testing for patterns
    I realize this isn't going to work on GitHub, but let's see how it
    breaks.
    
    It's a bit of a bummer those files are so big, but those should be the
    largest we ever have.

---
+++ e2e/patterns-page.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('patterns page / light mode', async ({ page }) => {
+  await page.goto('./patterns');
+  await expect(page).toHaveTitle(/Pattern Library \/ Cassette Nest/);
+  await expect(page).toHaveScreenshot('patterns-light.png', { fullPage: true });
+});
+
+test.use({ colorScheme: 'dark' });
+
+test('patterns page / dark mode', async ({ page }) => {
+  await page.goto('./patterns');
+  await expect(page).toHaveScreenshot('patterns-dark.png', { fullPage: true });
+});
