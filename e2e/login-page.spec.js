8676a1e954944fb6617e6e1f7499d94aa1b67363
Trey Piepmeier <trey@treypiepmeier.com>
2023-07-10 16:25:13 -0400

    Clean up what started as the example test

--- e2e/login-page.spec.js
+++ e2e/login-page.spec.js
@@ -4,14 +4,13 @@ import { test, expect } from '@playwright/test';
 // Don't use default, logged-in state
 test.use({ storageState: { cookies: [], origins: [] } });
 
-test('has title, go to create account', async ({ page }) => {
+test('log in and registration pages', async ({ page }) => {
   await page.goto('./');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Log in \/ Cassette Nest/);
+  await expect(page).toHaveTitle('Log in / Cassette Nest');
   await page
     .getByRole('link', { name: 'start your 14-day free trial' })
     .click();
   await expect(page).toHaveURL(/.*register/);
-  await page.screenshot({ path: 'test-results/login.png', fullPage: true });
 });
