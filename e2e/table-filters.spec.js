7b6f82fedb8cb37a4cf25d9fe3299a0dbc8ae424
Trey Piepmeier <trey@treypiepmeier.com>
2023-07-11 08:57:14 -0400

    Write the fabled failing test to catch that bug!

--- e2e/table-filters.spec.js
+++ e2e/table-filters.spec.js
@@ -3,6 +3,13 @@ import { test, expect } from '@playwright/test';
 
 test('stocks filter reset', async ({ page }) => {
   await page.goto('./stocks');
-
   await expect(page).toHaveTitle('Film Stocks / Cassette Nest');
+
+  await page
+    .getByRole('combobox', { name: 'Manufacturer:' })
+    .selectOption('kodak');
+  await page.getByRole('link', { name: 'Reset filters' }).click();
+  await expect(
+    page.getByRole('combobox', { name: 'Manufacturer:' })
+  ).toHaveValue('all');
 });
