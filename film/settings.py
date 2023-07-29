1c784bea803f2bfa94d0d9e50bc3253a33597ec4
Trey Piepmeier <trey@treypiepmeier.com>
2023-07-29 08:12:46 -0400

    Adjust things to be able to use Mailpit
    Run it like so:
    mailpit --smtp-auth-accept-any --smtp-auth-allow-insecure
    
    .env file:
    EMAIL_USE_SSL=0
    EMAIL_USE_TLS=0
    
    https://docs.djangoproject.com/en/stable/ref/settings/#std-setting-EMAIL_USE_SSL
    https://github.com/axllent/mailpit/wiki/SMTP-with-STARTTLS-and-authentication#smtp-authentication
    
    Learned about Mailpit via:
    https://bootcamp.laravel.com

--- film/settings.py
+++ film/settings.py
@@ -163,13 +163,13 @@ LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"
 
 # Email
-if DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = env("EMAIL_BACKEND")
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 SUBSCRIPTION_TRIAL = env.bool("SUBSCRIPTION_TRIAL", default=False)
