1a94004da15d8fe143183ac9f59d75ccca9ab61d
Trey Piepmeier <trey@treypiepmeier.com>
2023-09-22 17:46:44 -0400

    Get rid of console error
    `redeclaration of const confirmDelete`

--- inventory/templates/js/confirmation.js
+++ inventory/templates/js/confirmation.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-unused-vars */
-const confirmDelete = function (item = 'this item') {
+window.confirmDelete = function (item = 'this item') {
   return confirm(`Are you sure you want to delete this ${item}?`);
 };
 
-const confirmRemove = function (item = 'this item') {
+window.confirmRemove = function (item = 'this item') {
   return confirm(`Are you sure you want to remove ${item}?`);
 };
