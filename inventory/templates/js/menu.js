ee16ff2765ace0259690a630ce8234bcedb69eae
Trey Piepmeier <trey@treypiepmeier.com>
2023-09-23 09:55:34 -0400

    Different take on closing menu on back/forward
    piepworks/cassettenest-roadmap#322
    
    It was too jarring having the menu close as soon as you clicked.
    
    How to force reloading a page when using browser back button?
    https://stackoverflow.com/a/70546427

--- inventory/templates/js/menu.js
+++ inventory/templates/js/menu.js
@@ -6,13 +6,20 @@ window.cnMenu = {
   content: document.querySelector('#menu-content'),
   openButton: document.querySelector('#menu-open'),
   closeButtonWrapper: document.querySelector('#menu-close'),
-  get closeButton() { return this.closeButtonWrapper.querySelector('button'); },
+  get closeButton() {
+    return this.closeButtonWrapper.querySelector('button');
+  },
   toggleButton: document.querySelector('#menu-desktop-toggle'),
 
   setup: function () {
     this.base.classList.add('-translate-x-full', 'fixed');
     this.inner.classList.add('min-h-screen');
-    this.overlay.classList.add('opacity-0', 'fixed', 'inset-0', 'bg-opacity-75');
+    this.overlay.classList.add(
+      'opacity-0',
+      'fixed',
+      'inset-0',
+      'bg-opacity-75'
+    );
     this.content.classList.add('-translate-x-full', 'max-w-xs');
     this.openButton.classList.remove('hidden');
     this.openButton.classList.add('inline-flex');
@@ -23,20 +30,22 @@ window.cnMenu = {
     // switching and then using htmx's pushstate to go back and forward without
     // a full page reload. Hopefully most people won't see this, because there's
     // a bit of a blip when it happens.
-    document.removeEventListener('htmx:historyRestore', window.desktopToggleSetup);
+    document.removeEventListener(
+      'htmx:historyRestore',
+      window.desktopToggleSetup
+    );
     window.desktopToggleSetup = function () {
       fetch('{% url "session-sidebar-status" %}', {
-        headers: { 'X-Requested-With': 'XMLHttpRequest' }
-      }).then(
-        (response) => response.text()).then(
-        (text) => {
+        headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      })
+        .then((response) => response.text())
+        .then((text) => {
           if (text === 'closed') {
             window.cnMenu.inner.classList.add('collapsed');
           } else {
             window.cnMenu.inner.classList.remove('collapsed');
           }
-        }
-      );
+        });
     };
     document.addEventListener('htmx:historyRestore', window.desktopToggleSetup);
   },
@@ -74,7 +83,7 @@ window.cnMenu = {
 };
 
 document.body.removeEventListener('keyup', window.cnKeyupBackslash);
-window.cnKeyupBackslash = e => {
+window.cnKeyupBackslash = (e) => {
   if (e.code === 'Backslash') {
     window.cnMenu.desktopToggle();
   }
@@ -82,11 +91,18 @@ window.cnKeyupBackslash = e => {
 document.body.addEventListener('keyup', window.cnKeyupBackslash);
 
 document.body.removeEventListener('keyup', window.cnKeyupEsc);
-window.cnKeyupEsc = e => {
+window.cnKeyupEsc = (e) => {
   if (e.code === 'Escape') {
     window.cnMenu.close();
   }
 };
 document.body.addEventListener('keyup', window.cnKeyupEsc);
+
+// Close the menu on browser back or forward
+window.onpageshow = (e) => {
+  if (e.persisted) {
+    window.cnMenu.close();
+  }
+};
 
 window.cnMenu.setup();
