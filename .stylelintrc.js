db76d118a7f53bc9c3f5e796d9799f220b1cca61
Trey Piepmeier <trey@treypiepmeier.com>
2025-01-14 19:24:23 -0500

    Update stylelint and fix updated rules.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     ],
     'at-rule-empty-line-before': null,
     'value-keyword-case': null,
+    'at-rule-no-deprecated': null,
+    'declaration-property-value-no-unknown': null,
     // -----------------------
   },
   ignoreFiles: ['static/tailwind.css'],
